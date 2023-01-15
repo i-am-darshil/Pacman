9d39271b86b36bdd7eb7f0545f7f6be56f134f23
Darshil Shah <darshilx@amazon.com>
2023-01-15 10:42:34 +0530

    Create First boundary

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ let c = canvas.getContext('2d')
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+
+
+class Boundary {
+  constructor(position) {
+    this.position = position
+    this.width = 40
+    this.height = 40
+  }
+
+  draw() {
+    c.fillStyle = "blue"
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+let boundary = new Boundary({x: 0, y: 0})
+boundary.draw()
